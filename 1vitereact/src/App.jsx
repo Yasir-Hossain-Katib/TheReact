1ca406f37480a28c234e10edfcad7cf0a0e373bf
Yasir-Hossain-Katib <yasirkatib12@gmail.com>
2023-11-09 10:48:56 +0600

    gh

---
+++ 1vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Chai from "./chai";
+
+function App() {
+  
+
+  return (
+    <>
+    < Chai />
+    <h1>Chai aur react</h1>
+    <p>para</p> 
+
+    </>
+    
+  )
+}
+
+export default App
